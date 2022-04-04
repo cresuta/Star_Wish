ef59ccb6c88fe63ba3786fcf1975a4ff2118fbb3
cresuta <60229891+cresuta@users.noreply.github.com>
2022-04-04 11:20:27 -0400

    product and my wish list models created

---
+++ StarWish/Models/MyWishList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StarWish.Models
+{
+    public class MyWishList
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        public int NumberOfProducts { get; set; }
+
+        public int TotalCost { get; set; }
+
+        public int UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
---
+++ StarWish/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace StarWish.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string Condition { get; set; }
+        
+        //url to demo product listing on eBay if user wants additional item details
+        public string ItemWebUrl { get; set; }
+
+        public int MyWishListId { get; set; }
+        public MyWishList MyWishList { get; set; }
+    }
+}
