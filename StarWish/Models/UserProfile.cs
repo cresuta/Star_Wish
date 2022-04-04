04979610beb304369ea71e319336c594f963eade
cresuta <60229891+cresuta@users.noreply.github.com>
2022-04-04 10:51:55 -0400

    user profile model created

---
+++ StarWish/Models/UserProfile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StarWish.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string DisplayName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(255)]
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [DataType(DataType.Url)]
+        [MaxLength(255)]
+        public string ProfileImage { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        public string FullName
+        {
+            get 
+            {
+                return $"{FirstName} {LastName}";
+                
+            }
+        }
+
+    }
+}
