920cfd4308bfeba16f77880585b374b2eb78679e
cresuta <60229891+cresuta@users.noreply.github.com>
2022-04-08 12:03:57 -0400

    working on navbar

--- StarWish/client/src/components/Header.js
+++ StarWish/client/src/components/Header.js
@@ -4,64 +4,32 @@ import {
   Container,
   Nav,
   NavDropdown,
-  Offcanvas,
-  Form,
-  FormControl,
-  Button,
+ 
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { PersonCircle } from "react-bootstrap-icons";
 
 export default function Header() {
   return (
-    <Navbar className="navbar" bg="light" expand={false}>
-      <Container fluid>
-        <Navbar.Brand href="#">
-          <img
-            src={require("../imgs/app-logo.png")}
-            width="200"
-            height="200"
-            className="d-inline-block align-top navbar__img"
-            alt="React Bootstrap logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
-        <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          placement="end"
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">
-              Offcanvas
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+    <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Welcome back, Cameron!</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse className="navbar-rightside" id="basic-navbar-nav">
+      <Nav className="me-auto">
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+       
+        <i class="bi bi-cart2"></i>       
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 }
