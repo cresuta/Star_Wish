8ae817aa0c22b5e6bf876bdab633997a0659e4f2
cresuta <60229891+cresuta@users.noreply.github.com>
2022-04-11 12:01:06 -0400

    current user name added to welcome display in navbar

--- StarWish/client/src/components/Header.js
+++ StarWish/client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { UserProfileContext } from "../providers/UserProfileProvider";
@@ -6,7 +6,10 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header() {
 
+  const currentUser = JSON.parse(sessionStorage.getItem("userProfile"));
   const {logout} = useContext(UserProfileContext);
+  const { isLoggedIn } = useContext(UserProfileContext);
+
   const navigate = useNavigate();
   const signOut = (e) => {
     e.preventDefault();
@@ -14,28 +17,31 @@ export default function Header() {
     navigate("/login");
   }
 
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Welcome back, Cameron!</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="navbar-rightside" id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavDropdown title="My StarWish" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/">Dashboard</NavDropdown.Item>
-              <NavDropdown.Item href="/wishlists">
-               Wish Lists
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item onClick={signOut}>
-                Sign Out
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <i class="bi bi-cart2" id="shopping-cart"></i>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  if (isLoggedIn) {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>Welcome back, <span id="welcome-displayname">{currentUser.firstName}</span>!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="navbar-rightside" id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="My StarWish" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/">Dashboard</NavDropdown.Item>
+                <NavDropdown.Item href="/wishlists">
+                 Wish Lists
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={signOut}>
+                  Sign Out
+                </NavDropdown.Item>
+              </NavDropdown>
+  
+              <i class="bi bi-cart2" id="shopping-cart"></i>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+  
 }
--- StarWish/client/src/providers/UserProfileProvider.js
+++ StarWish/client/src/providers/UserProfileProvider.js
@@ -9,7 +9,6 @@ export function UserProfileProvider(props) {
   const userProfile = sessionStorage.getItem("userProfile");
   const [isLoggedIn, setIsLoggedIn] = useState(userProfile != null);
 
-
   const login = (userObject) => {
     return fetch(`${apiUrl}/api/userprofile/getbyemail?email=${userObject.email}`)
     .then((r) => r.json())
@@ -51,7 +50,7 @@ export function UserProfileProvider(props) {
 
 
   return (
-    <UserProfileContext.Provider value={{ isLoggedIn, login, logout, register,  }}>
+    <UserProfileContext.Provider value={{ isLoggedIn, login, logout, register}}>
        {props.children}
     </UserProfileContext.Provider>
   );
