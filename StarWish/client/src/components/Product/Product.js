29b86f88115c1e8b75599e53b6138418424a1fbe
cresuta <60229891+cresuta@users.noreply.github.com>
2022-04-12 16:34:59 -0400

    layout/styling done on product list and each product, need to fix overall search results container size

--- StarWish/client/src/components/Product/Product.js
+++ StarWish/client/src/components/Product/Product.js
@@ -1,26 +1,34 @@
 import React from "react";
 
 export const Product = ({ product }) => {
-  console.log(product.image.imageUrl);
   return (
     <section className="product-wrapper">
       <div className="product-img-container">
-      <img
-        src={product?.image.imageUrl}
-        alt="Hello"
-        className="product-img"
-      />
+        <img
+          src={product?.image.imageUrl}
+          alt="Hello"
+          className="product-img"
+        />
       </div>
       <div className="product-info">
-          <a target="_blank" rel="noreferrer" href={product?.itemWebUrl}><h3 className="product-title">{product?.title}</h3></a>
-          <div className="product-subtitle">Brand {product?.condition}</div>
-          <div className="product-details">
-              <p className="product-price">${product?.price.value}</p>
-              <p className="product-offer">Buy It Now</p>
-
-          </div>
-          <span id="star-icon-add-to-cart"><i class="bi bi-star"></i></span>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="product-title"
+          href={product?.itemWebUrl}
+        >
+          <h3 className="product-title2">{product?.title}</h3>
+        </a>
+        <div className="product-subtitle">Brand {product?.condition}</div>
+        <div className="product-details">
+          <p className="product-price">${product?.price.value}</p>
+          <p className="product-offer">Buy It Now</p>
+          <p className="product-shipping">Free Shipping</p>
+        </div>
       </div>
+      <span id="star-icon-add-to-cart">
+        <i class="bi bi-star"></i>
+      </span>
     </section>
   );
 };
